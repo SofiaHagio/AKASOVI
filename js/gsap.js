7074f671856b5e67868f4d4d9d827dc1c3361d14
Sofia Hagio <sofia.hagio@gmail.com>
2026-04-16 00:15:38 -0300

    Add files via upload

---
+++ js/gsap.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".containerPai",
+      start: "top top",
+      end: "+=2000",
+      onLeave: () => (window.location.href = "index.html"),
+      markers: true,
+      pin: true,
+      scrub: 2,
+    },
+  });
+
+  tl.to(".secao1", { WebkitMaskSize: "20vw", maskSize: "20vw", duration: 2 });
+  tl.to(".secaoBranca", { backgroundColor: "white", duration: 1 }, "-=1");
+  tl.from(".secao2 img", { opacity: 0, filter: "blur(20px)", duration: 0.5 }, "+=.3");
+
+  ScrollTrigger.refresh();
+});
