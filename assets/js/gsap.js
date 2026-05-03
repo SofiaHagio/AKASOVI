46c7b9be41c91667d9372749c6235db25436a27a
Sofia Hagio <sofia.hagio@gmail.com>
2026-05-03 14:05:57 -0300

    update

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -2,22 +2,14 @@ window.addEventListener("load", () => {
   gsap.registerPlugin(ScrollTrigger);
  
   const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".containerPai",
-      start: "top top",
-      end: "+=300%",
-      onLeaveBack: () => {},   
-      onLeave: () => {
-        window.location.href = "sobre.html";  
-      },
-      pin: true,
-      scrub: 2,
-    },
+    onComplete: () => {
+      window.location.href = "sobre.html";
+    }
   });
  
   tl.to(".secao1", { WebkitMaskSize: "20vw", maskSize: "20vw", duration: 2 });
   tl.to(".secaoBranca", { backgroundColor: "white", duration: 1 }, "-=1");
   tl.from(".secao2 img", { opacity: 0, filter: "blur(20px)", duration: 0.5 }, "+=.3");
- 
-  ScrollTrigger.refresh();
+  tl.to(".secao2 img", { opacity: 0, duration: 0.4 }, "+=0.8");
 });
+ 
